c08d4aa5d8c692ffcf4847019de02afdadc918f3
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2019-08-18 18:13:17 -0300

    Adds formating to StringObserver

---
+++ Assets/SO Architecture/Editor/Inspectors/StringObserverEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+namespace ScriptableObjectArchitecture.Editor
+{
+    [CustomEditor(typeof(StringObserver), true)]
+    public class StringObserverEditor : BaseObserverEditor
+    {
+        private StringObserver Target { get { return (StringObserver)target; } }
+        private SerializedProperty _format;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _format = serializedObject.FindProperty("_format");
+        }
+
+        protected override void DrawGameEventField()
+        {
+            base.DrawGameEventField();
+            EditorGUILayout.PropertyField(_format);
+        }
+    }
+}
