20142ee65e6f9e4e12a9b09f09bf441dc2b25325
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2019-08-18 15:36:13 -0300

    RandomFloatVariable

---
+++ Editor/Inspectors/ReadOnlyVariableEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ScriptableObjectArchitecture.Editor
+{
+
+    [CustomEditor(typeof(ReadOnlyVariable<>), true)]
+    public class ReadOnlyVariableEditor : BaseVariableEditor
+    {
+
+        private BaseVariable Target { get { return (BaseVariable)target; } }
+        
+        protected override void DrawValue()
+        {
+            string content = "Cannot display value. No PropertyDrawer for (" + Target.Type + ") [" + Target.ToString() + "]";
+
+            using (var scope = new EditorGUI.DisabledGroupScope(true))
+            {
+                GenericPropertyDrawer.DrawPropertyDrawerLayout(Target.Type, new GUIContent("Value"), _valueProperty, new GUIContent(content, content));
+            }
+
+            var obj = Target.BaseValue;
+        }
+
+        protected override void DrawReadonlyField()
+        {
+
+        }
+    }
+}
