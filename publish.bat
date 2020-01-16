6f18ed2db17ebfebd2dba97a8098f5e15c9c424d
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-01-16 16:12:50 -0300

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
-set /p version="Versão: "
+set /p version="Version: "
 git subtree split --prefix="Assets/SO Architecture" --branch upm
-git tag version upm
+git tag %version% upm
 git push origin upm --tags
 PAUSE
