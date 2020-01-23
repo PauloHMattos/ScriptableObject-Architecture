180ac57082822054f31f28ce3613461ee5758e5a
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-01-23 11:03:05 -0300

    Editor test

---
+++ Assets/SO Architecture/Tests/EventsTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    //public class EventsTests
+    //{
+    //    private GameObject testRunnerGameObject;
+    //    private GameEventRaiser eventRaiser;
+
+    //    [Test, Performance]
+    //    public void GameEventInvokeTest([Values(1, 10, 100, 1000)] int count)
+    //    {
+    //        //Measure.Method(() => eventRaiser.Raise()).WarmupCount(10).IterationsPerMeasurement(count).GC().Run();
+    //    }
+
+    //    [Test, Performance]
+    //    public void GameEventInvokeTestNoWarmup([Values(1, 10, 100, 1000)] int count)
+    //    {
+    //        Measure.Method(() => eventRaiser.Raise()).IterationsPerMeasurement(count).GC().Run();
+    //    }
+
+    //    [OneTimeSetUp]
+    //    public void Setup()
+    //    {
+    //        SceneManager.LoadScene("GameEventTestScene", LoadSceneMode.Single);
+    //        testRunnerGameObject = GameObject.FindGameObjectWithTag("Player");
+    //        eventRaiser = testRunnerGameObject.GetComponent<GameEventRaiser>();
+    //    }
+    //}
+}
---
+++ Assets/Tests/EventsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EventsTests
+    {
+        private GameObject testRunnerGameObject;
+        private GameObject listenerGameObject;
+        private GameEventRaiser eventRaiser;
+
+        [UnityTest, Performance]
+        public IEnumerator GameEventInvokeTest([Values(1, 2, 10, 100, 1000)] int count)
+        {
+            Measure.Method(() => eventRaiser.Raise()).WarmupCount(0).MeasurementCount(1).IterationsPerMeasurement(count).GC().Run();
+        }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            EditorSceneManager.OpenScene(Application.dataPath+ "/Tests/Scenes/GameEventTestScene.unity", OpenSceneMode.Single);
+            testRunnerGameObject = GameObject.FindGameObjectWithTag("Player");
+            eventRaiser = testRunnerGameObject.GetComponent<GameEventRaiser>();
+            listenerGameObject = GameObject.Find("Listener");
+        }
+    }
+}
