b87d5d3a5425ca5f51f57a4c6f896fe74876cf02
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-01-25 21:58:08 -0300

    Reimplements ReadOnlyAttribute using propertydrawers

---
+++ Assets/SO Architecture/Editor/Drawers/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ScriptableObjectArchitecture.Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            ReadOnlyAttribute attr = (ReadOnlyAttribute)attribute;
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.PropertyField(rect, property, label, true);
+            EditorGUI.EndDisabledGroup();
+        }
+
+        //public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        //{
+        //    return EditorGUIUtility.singleLineHeight;
+        //}
+    }
+}
